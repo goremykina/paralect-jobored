3e0912191e1552ca18a7f12c1d08ed3de995e769
Alina Goremykina <a.goremykina98@gmail.com>
2023-05-22 23:10:19 +0200

    Implement refresh token functionality

--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -9,32 +9,40 @@ type AuthorizationResponse = {
     ttl: number;
     expires_in: number;
     token_type: string;
-    reg_user_resumes_count: number;
 };
 
 const api = axios.create(config);
-const authorizeQueryParams = {
-    login: 'sergei.stralenia@gmail.com',
-    password: 'paralect123',
+const baseQueryParams = {
     client_id: '2356',
     client_secret: 'v3.r.137440105.ffdbab114f92b821eac4e21f485343924a773131.06c3bdbb8446aeb91c35b80c42ff69eb9c457948',
+};
+const authorizeQueryParams = {
+    ...baseQueryParams,
+    login: 'sergei.stralenia@gmail.com',
+    password: 'paralect123',
     hr: '0'
 };
 const cookies = new Cookies();
 
 export const getAccessToken = async () => {
     const accessTokenCookie = cookies.get('accessToken');
-
     if (accessTokenCookie) {
         return accessTokenCookie;
     }
 
-    const { data } = await api.get<AuthorizationResponse>('/oauth2/password', { params: authorizeQueryParams });
+    const refreshTokenCookie = cookies.get('refreshToken');
+    const { data } = refreshTokenCookie
+        ? await api.get<AuthorizationResponse>('/oauth2/refresh_token', { params: { ...baseQueryParams, refresh_token: refreshTokenCookie } })
+        : await api.get<AuthorizationResponse>('/oauth2/password', { params: authorizeQueryParams });
     const accessToken = `${data.token_type} ${data.access_token}`;
 
     cookies.set('accessToken', accessToken, {
         path: '/',
         expires: fromUnixTimestamp(data.ttl)
+    });
+
+    cookies.set('refreshToken', data.refresh_token, {
+        path: '/'
     });
 
     return accessToken;
